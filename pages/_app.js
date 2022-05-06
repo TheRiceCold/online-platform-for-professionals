7deac1cdac1eedca42565fc42b4cd321a7b7e713
Walter <dalewaltergh@gmail.com>
2022-05-07 03:35:09 +0800

    registration form and validation schema

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
 import '../styles/globals.css'
 
-const MyApp = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />
-}
+const MyApp = ({ Component, pageProps }) => (
+  <Component {...pageProps} />
+)
 
 export default MyApp
