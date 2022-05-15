a7749127b8ff9315c04b1c9acde6ad2ce3f229a7
Walter <dalewaltergh@gmail.com>
2022-05-16 01:36:25 +0800

    professionals context actions

--- src/context/professional/actions.js
+++ src/context/professional/actions.js
@@ -2,25 +2,36 @@ import Axios from "@/utils/axios"
 
 const Actions = user => {
   const {token} = user
+  const path = "professionals/"
 
   return {
     // GET
     getAll: async () => { 
-
+      return Axios.get(path, {
+        headers: { 'Authorization': token }
+      })
     },
     getById: async id => { 
-
+      return Axios.get(path+id, {
+        headers: { 'Authorization': token }
+      })
     },
 
     // MUTATIONS
     create: async data => { 
-
+      return Axios.post(path, data, {
+        headers: { 'Authorization': token }
+      })
     },
     update: async (id, data) => {
-
+      return Axios.patch(path+id, data, {
+        headers: { 'Authorization': token }
+      })
     },
     delete: async id => {
-
+      return Axios.delete(path+id, {
+        headers: { 'Authorization': token }
+      })
     }
   }
 }
--- src/layouts/professional/Layout.jsx
+++ src/layouts/professional/Layout.jsx
@@ -1,6 +1,12 @@
 import {
-  Avatar, Flex, 
-  Box, Text, Link
+  Flex, 
+  Box, 
+  Text, 
+  Link,
+  Avatar, 
+  Skeleton, 
+  SkeletonText,
+  SkeletonCircle,
 } from "@chakra-ui/react"
 import Navbar from "@/components/navbars/Navbar"
 
@@ -14,27 +20,34 @@ const ProfessionalLayout = () => {
         <Box height="50%" bg="gray.400"/>
         <Flex justify="center" flexGrow={1}>
           <Flex w="95%">
-            <Avatar 
-              size="2xl" 
-              top={-3}
-              position="relative"
-              borderRadius="full"
-              border="6px solid white"
-              src="https://avatars.dicebear.com/api/male/username.svg" 
-            />
+            <SkeletonCircle size="116px"/>
+            {/* <Avatar  */}
+            {/*   size="2xl"  */}
+            {/*   top={-3} */}
+            {/*   position="relative" */}
+            {/*   borderRadius="full" */}
+            {/*   border="6px solid white" */}
+            {/*   src="https://avatars.dicebear.com/api/male/username.svg"  */}
+            {/* /> */}
             <Flex padding="10px 16px">
               <Flex direction="column" mr={6}>
-                <Text fontWeight={700} fontSize="26px">
-                  FullName
-                </Text>
-                <Text fontSize="16px">
-                  Professional 
-                </Text>
-                <Text fontSize="14px" color="gray.600">
-                  <Link color="blue.500" fontWeight={500}>
-                    Contact Info
-                  </Link>
-                </Text>
+                <Skeleton h="30px" mb={2}>
+                  <Text fontWeight={700} fontSize="26px">
+                    first name last name
+                  </Text>
+                </Skeleton>
+                <Skeleton h="20px" mb={2}>
+                  <Text fontSize="16px">
+                    Programmer
+                  </Text>
+                </Skeleton>
+                <Skeleton h="18px">
+                  <Text fontSize="14px" color="gray.600">
+                    <Link color="blue.500" fontWeight={500}>
+                      Contact Info
+                    </Link>
+                  </Text>
+                </Skeleton>
               </Flex>
             </Flex>
           </Flex>
