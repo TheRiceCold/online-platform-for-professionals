5cd5c0f265c7bee8981dab27f8dd16af0c818246
Godfrey Peralta <godfreydjperalta@gmail.com>
2022-05-21 12:36:04 +0800

    Add key for mockUsers.map and add TODO comments

--- src/components/search/ProfessionalOverview.jsx
+++ src/components/search/ProfessionalOverview.jsx
@@ -1,5 +1,23 @@
 import styles from '@/styles/Components.module.sass';
-import { Box, SkeletonCircle, Avatar, Text, Button } from '@chakra-ui/react';
+import {
+	Box,
+	SkeletonCircle,
+	Avatar,
+	Text,
+	Button,
+	ButtonGroup,
+	UnorderedList,
+	ListItem,
+} from '@chakra-ui/react';
+import { StarIcon, CalendarIcon, AddIcon } from '@chakra-ui/icons';
+
+// TODO Insert selected professional data
+// TODO REMOVE after adding context
+import {
+	services,
+	workPortfolios,
+	reviews,
+} from '../../temporaryMocks/mock_professional_data';
 
 const ProfessionalOverview = ({ img, isLoading }) => {
 	return (
@@ -27,15 +45,64 @@ const ProfessionalOverview = ({ img, isLoading }) => {
 					</Box>
 				</Box>
 				<Box className={styles.actions}>
-					{/* Call post /connections */}
-					<Button className={styles.subscribe}>Subscribe</Button>
+					<ButtonGroup spacing={3}>
+						{/*TODO Call post /connections */}
+						<Button leftIcon={<AddIcon />} className={styles.subscribe}>
+							Subscribe
+						</Button>
+						{/* TODO Disable button if not subscribed (client.subscription.includes(professional)) */}
+						<Button
+							leftIcon={<CalendarIcon boxSize={5} />}
+							className={styles.bookEvent}
+						>
+							Easy Book
+						</Button>
+					</ButtonGroup>
 				</Box>
 			</Box>
 			<Box className={styles.overview}>
-				<Text fontSize="2xl">Headline</Text>
+				<Text fontSize="2xl">Headline text here</Text>
 				<Text fontSize="2xl">Services</Text>
+				<UnorderedList spacing={3}>
+					{services.map((service) => {
+						return (
+							<ListItem color="white">
+								<Text>{service.title}</Text>
+								<Text>{service.details}</Text>
+								{service.minPrice ? (
+									<Text as="i">
+										Php {service.minPrice}-{service.maxPrice}
+									</Text>
+								) : (
+									''
+								)}
+							</ListItem>
+						);
+					})}
+				</UnorderedList>
 				<Text fontSize="2xl">Work Portfolio</Text>
+				<UnorderedList>
+					{workPortfolios.map((workPortfolio) => {
+						return (
+							<ListItem color="white">
+								<Text>{workPortfolio.title}</Text>
+								<Text>{workPortfolio.details}</Text>
+							</ListItem>
+						);
+					})}
+				</UnorderedList>
 				<Text fontSize="2xl">Reviews</Text>
+				{reviews.map((review) => {
+					return (
+						<Box color="white">
+							{[...Array(5)].map((n, i) => {
+								return <StarIcon color={i + 1 < review.rating ? '#ff652f' : 'gray'} />;
+							})}
+							<Text>{review.rating}</Text>
+							<Text>{review.body}</Text>
+						</Box>
+					);
+				})}
 			</Box>
 		</Box>
 	);
