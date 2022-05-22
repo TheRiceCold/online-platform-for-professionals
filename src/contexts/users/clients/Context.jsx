843d997b39749ebdfae35c26ac07124f05fcb040
Godfrey Peralta <godfreydjperalta@gmail.com>
2022-05-22 11:01:54 +0800

    Correct capitalization of paths

--- src/contexts/users/clients/Context.jsx
+++ src/contexts/users/clients/Context.jsx
@@ -1,37 +1,36 @@
-import Actions from "./actions"
+import Actions from './Actions';
 
-import {navLinks} from "./navLinks"
-import {createContext} from "react"
-import {useRouter} from "next/router"
-import {useAuth} from "../../auth/Context"
-import {userMenuItems} from "./userMenuItems"
+import { navLinks } from './navLinks';
+import { createContext } from 'react';
+import { useRouter } from 'next/router';
+import { useAuth } from '../../auth/Context';
+import { userMenuItems } from './userMenuItems';
 
-const ClientsContext = createContext()
+const ClientsContext = createContext();
 
-const ClientsProvider = ({children}) => {
-  const {Provider} = ClientsContext
-  const {user, logout} = useAuth()
-  const router = useRouter()
-  const call = new Actions(user)
+const ClientsProvider = ({ children }) => {
+	const { Provider } = ClientsContext;
+	const { user, logout } = useAuth();
+	const router = useRouter();
+	const call = new Actions(user);
 
-  const menuItems = openSettings => 
-    userMenuItems(user, router, logout, openSettings)
+	const menuItems = (openSettings) =>
+		userMenuItems(user, router, logout, openSettings);
 
-  return (
-    <Provider value={{
-      menuItems,
-      navLinks: navLinks(user.clientId),
+	return (
+		<Provider
+			value={{
+				menuItems,
+				navLinks: navLinks(user.clientId),
 
-      getClients: call.getAll,
-      getClient: call.getById,
-      deleteClient: call.delete,
-    }}>
-      {children}
-    </Provider>
-  )
-}
+				getClients: call.getAll,
+				getClient: call.getById,
+				deleteClient: call.delete,
+			}}
+		>
+			{children}
+		</Provider>
+	);
+};
 
-export {
-  ClientsContext,
-  ClientsProvider
-}
+export { ClientsContext, ClientsProvider };
