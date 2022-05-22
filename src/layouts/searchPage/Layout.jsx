37d80c2650f2416cc112bd1b799dfb9fa7d25db5
Godfrey Peralta <godfreydjperalta@gmail.com>
2022-05-22 11:06:14 +0800

    Remove Navbar component and the variables it depended on

--- src/layouts/searchPage/Layout.jsx
+++ src/layouts/searchPage/Layout.jsx
@@ -1,21 +1,14 @@
 import styles from '@/styles/Professionals.module.sass';
 
-import Navbar from '../navbar/Navbar';
-import { useUsers } from '@/context/users/Context';
+import { useUsers } from '@/contexts/users/Context';
 import ProfessionalOverview from './ProfessionalOverview';
 import SearchResultNavigation from './SearchResultNavigation';
 
-const SearchLayout = ({ fullname }) => {
-	const { navLinks, userMenuItems, userImg, isLoading } = useUsers('client');
+const SearchLayout = () => {
+	const { userImg, isLoading } = useUsers('client');
 
 	return (
 		<>
-			<Navbar
-				styles={styles}
-				fullname={fullname}
-				userMenuItems={userMenuItems}
-				links={navLinks}
-			/>
 			<section className={styles.searchResultsLayout}>
 				<SearchResultNavigation img={userImg} isLoading={isLoading} />
 				{/*TODO Remove img and isloading when link route is available */}
