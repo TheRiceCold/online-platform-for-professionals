c688a659a9ce4d36b6af8952f367a43aef6b0251
Walter <dalewaltergh@gmail.com>
2022-05-24 05:16:34 +0800

    connection modals setup
    - clientele modal
    - subscribers modal

---
+++ src/layouts/navbar/modals/ClienteleModal.jsx
@@ -0,0 +1,15 @@
+import Modal from "@/components/over/Modal"
+
+function ClienteleModal({...props}) {
+
+  return (
+    <Modal
+      {...props}
+      header="Clietele"
+    >
+      
+    </Modal>
+  )
+}
+
+export default ClienteleModal
---
+++ src/layouts/navbar/modals/SubscribersModal.jsx
@@ -0,0 +1,16 @@
+import Modal from "@/components/over/Modal"
+
+function SubscribersModal({...props}) {
+
+  return (
+    <Modal
+      {...props}
+      header="Subscribers"
+    >
+      
+    </Modal>
+  )
+}
+
+export default SubscribersModal
+
