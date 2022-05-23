570dae01dd64591a807ceea2f72893cdde3c78c9
Godfrey Peralta <godfreydjperalta@gmail.com>
2022-05-23 20:22:10 +0800

    Add mock data for links

---
+++ src/lib/data/mock_links.js
@@ -0,0 +1,17 @@
+let professionalLinks = {
+	self: 'http://localhost:3001/professionals?page=1',
+	first: 'http://localhost:3001/professionals?page=1',
+	next: 'http://localhost:3001/professionals?page=2',
+	// prev: 'http://localhost:3001/professionals?page=7',
+	last: 'http://localhost:3001/professionals?page=8',
+};
+
+let bookingLinks = {
+	self: 'http://localhost:3001/bookings?page=5',
+	first: 'http://localhost:3001/bookings?page=1',
+	next: 'http://localhost:3001/bookings?page=6',
+	prev: 'http://localhost:3001/bookings?page=4',
+	last: 'http://localhost:3001/bookings?page=8',
+};
+
+export { professionalLinks, bookingLinks };
