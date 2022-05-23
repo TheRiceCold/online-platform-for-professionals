0df301da5a46585003e8040d1e25b8f61488e220
Godfrey Peralta <godfreydjperalta@gmail.com>
2022-05-23 20:06:30 +0800

    Update path of styles and layout

--- src/pages/professionals/[professional_id]/bookings/index.js
+++ src/pages/professionals/[professional_id]/bookings/index.js
@@ -1,25 +1,25 @@
-import styles from "@/styles/Bookings.module.sass"
+import styles from '@/styles/Bookings.module.sass';
 
-import Head from "next/head"
-import Navbar from "@/layouts/navbar/Navbar"
-import Layout from "@/layouts/bookings/Layout"
+import Head from 'next/head';
+import Navbar from '@/layouts/navbar/Navbar';
+import Layout from '@/layouts/users/bookings/Layout';
 
-import {useAuth} from "@/auth_context"
-import {useUsers} from "@/users_context"
+import { useAuth } from '@/auth_context';
+import { useUsers } from '@/users_context';
 
 function Bookings() {
-  const {userRole, userFullname} = useAuth()
-  const {navLinks} = useUsers(userRole)
+	const { userRole, userFullname } = useAuth();
+	const { navLinks } = useUsers(userRole);
 
-  return (
-    <main className={styles.main}>
-      <Head>
-        <title>{userFullname} | Bookings</title>
-      </Head>
-      <Navbar styles={styles} links={navLinks}/>
-      <Layout/>
-    </main>
-  )
+	return (
+		<main className={styles.main}>
+			<Head>
+				<title>{userFullname} | Bookings</title>
+			</Head>
+			<Navbar styles={styles} links={navLinks} />
+			<Layout />
+		</main>
+	);
 }
 
-export default Bookings
+export default Bookings;
