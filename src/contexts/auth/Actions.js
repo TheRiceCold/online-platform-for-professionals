6ff61782d71933572371adda040d463904af1e6a
Godfrey Peralta <godfreydjperalta@gmail.com>
2022-05-24 16:44:32 +0800

    Add config to update

--- src/contexts/auth/Actions.js
+++ src/contexts/auth/Actions.js
@@ -1,48 +1,47 @@
-import Axios from "@/axios"
-import {useStorage} from "@/hooks/useStorage"
+import Axios from '@/axios';
+import { useStorage } from '@/hooks/useStorage';
 
 function Actions(dispatch, token) {
-  const storage = useStorage()
-  const config = { headers: { Authorization: token } }
+	const storage = useStorage();
+	const config = { headers: { Authorization: token } };
 
-  this.signup = async data => 
-    await Axios.post("signup", this.signupData(data))
+	this.signup = async (data) =>
+		await Axios.post('signup', this.signupData(data));
 
-  this.login = async data => 
-    await Axios.post("login", { user: {...data} })
+	this.login = async (data) => await Axios.post('login', { user: { ...data } });
 
-  this.update = async data => 
-    await Axios.patch("signup", this.signupData(data))
+	this.update = async (data) =>
+		await Axios.patch('signup', this.signupData(data), config);
 
-  this.logout = async () => {
-    await Axios.delete("logout", config)
-    dispatch({type: "LOGOUT"})
-    storage.removeItem({ 
-      type: "session", 
-      key: "auth_data" 
-    })
-    location.reload()
-  }
+	this.logout = async () => {
+		await Axios.delete('logout', config);
+		dispatch({ type: 'LOGOUT' });
+		storage.removeItem({
+			type: 'session',
+			key: 'auth_data',
+		});
+		location.reload();
+	};
 
-  // private
-  this.signupData = data => {
-    let contactNo = data.contact_number
+	// private
+	this.signupData = (data) => {
+		let contactNo = data.contact_number;
 
-    switch (contactNo.charAt(0)) {
-      case "0": // allow 09 format
-        contactNo = contactNo.substring(1) 
-        break
-      case "6": // allow 639 formant
-        contactNo = contactNo.substring(2) 
-        break
-      case "+": // allow +639 format
-        contactNo = contactNo.substring(3) 
-        break
-    }
+		switch (contactNo.charAt(0)) {
+			case '0': // allow 09 format
+				contactNo = contactNo.substring(1);
+				break;
+			case '6': // allow 639 formant
+				contactNo = contactNo.substring(2);
+				break;
+			case '+': // allow +639 format
+				contactNo = contactNo.substring(3);
+				break;
+		}
 
-    data.contact_number = contactNo
-    return { user: {...data} }
-  }
+		data.contact_number = contactNo;
+		return { user: { ...data } };
+	};
 }
 
-export default Actions
+export default Actions;
