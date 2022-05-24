529b88bca0912de7017c9d8be31cd3d43f508e98
Godfrey Peralta <godfreydjperalta@gmail.com>
2022-05-24 12:21:43 +0800

    Add professional name user.meta.name at line 36 Add conditional render of user.meta.averageRating

--- src/layouts/users/professionals/search/sidebar/ProfileDescription.jsx
+++ src/layouts/users/professionals/search/sidebar/ProfileDescription.jsx
@@ -1,24 +1,13 @@
-import styles from "@/styles/users/Professionals.module.sass"
+import styles from '@/styles/users/Professionals.module.sass';
 
-import {
-	Avatar,
-	Box, Text,
-	SkeletonCircle,
-} from "@chakra-ui/react"
-import {StarIcon} from "@chakra-ui/icons"
+import { Avatar, Box, Text, SkeletonCircle } from '@chakra-ui/react';
+import { StarIcon } from '@chakra-ui/icons';
 
 function ProfileDescription(props) {
-  const {
-    user, img, 
-    isLoading, 
-    setSelectedId,
-  } = props
+	const { user, img, isLoading, setSelectedId } = props;
 
 	return (
-    <Box 
-      className={styles.description}
-      onClick={() => setSelectedId(user.id)}
-    >
+		<Box className={styles.description} onClick={() => setSelectedId(user.id)}>
 			<Box className={styles.userInfo}>
 				<SkeletonCircle size="116px" isLoaded={!isLoading}>
 					<Avatar
@@ -33,23 +22,27 @@ function ProfileDescription(props) {
 				</SkeletonCircle>
 				<Box>
 					<Text className={styles.field}>{user.attributes.field}</Text>
-          <Text className={styles.name}>{user.name}</Text>
+					<Text className={styles.name}>{user.meta.name}</Text>
 					<Text>{user.attributes.headline}</Text>
 					<Text>{user.attributes.officeAddress}</Text>
 				</Box>
 			</Box>
 			<Box className={styles.metrics} color="gray.500">
-				<Box className={styles.rating}>
-					<Text fontSize="lg">{user.meta.averageRating}</Text>
-					<Box paddingLeft="0.3rem">
-						<StarIcon color="#ffe400" />
+				{user.meta.averageRating ? (
+					<Box className={styles.rating}>
+						<Text fontSize="lg">{user.meta.averageRating}</Text>
+						<Box paddingLeft="0.3rem">
+							<StarIcon color="#ffe400" />
+						</Box>
 					</Box>
-				</Box>
+				) : (
+					<Text>No ratings</Text>
+				)}
 				<Text>{user.relationships.subscribers.data.length} subscribers</Text>
 				<Text>{user.relationships.clientele.data.length} clients</Text>
 			</Box>
 		</Box>
-	)
+	);
 }
 
-export default ProfileDescription
+export default ProfileDescription;
