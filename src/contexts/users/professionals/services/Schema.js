70ca44d227692cbded0565a8ac614398ddb307c0
Godfrey Peralta <godfreydjperalta@gmail.com>
2022-05-24 15:02:38 +0800

    BUG: Comment out refine lines 10 to 13. For FIXING. invalidation not disappearing when corrected

--- src/contexts/users/professionals/services/Schema.js
+++ src/contexts/users/professionals/services/Schema.js
@@ -1,19 +1,15 @@
-import {z} from "zod"
+import { z } from 'zod';
 
 // MUST HAVE THE SAME NAME AS THE INPUT IDS
 const Schema = z.object({
-  title: z.string()
-    .min(1, { message: "Title can't be empty"}),
-  details: z.string()
-    .min(1, { message: "Details can't be empty"}),
-  min_price: z.string()
-    .min(1, { message: "Minimum required"}),
-  max_price: z.string()
-    .min(1, { message: "Maximum required"}),
-})
-.refine((data) => Number(min_price) < Number(max_price), {
-  message: "Max price should be higher than minimun price",
-  path: ["max_price"], // path of error
-})
+	title: z.string().min(1, { message: "Title can't be empty" }),
+	details: z.string().min(1, { message: "Details can't be empty" }),
+	min_price: z.string().min(1, { message: 'Minimum required' }),
+	max_price: z.string().min(1, { message: 'Maximum required' }),
+});
+// .refine((data) => Number(min_price) < Number(max_price), {
+//   message: "Max price should be higher than minimun price",
+//   path: ["max_price"], // path of error
+// })
 
-export default Schema
+export default Schema;
