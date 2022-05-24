c6f2aad3267324f91b6047fc9914504b47cab1b8
Godfrey Peralta <godfreydjperalta@gmail.com>
2022-05-24 15:03:05 +0800

    Add navbar options for professional

--- pages/professionals/index.js
+++ pages/professionals/index.js
@@ -3,29 +3,31 @@ import styles from '@/styles/users/Professionals.module.sass';
 import Head from 'next/head';
 import Navbar from '@/layouts/navbar/Navbar';
 import Layout from '@/professionals_layout/search/Layout';
+import ClienteleModal from '@/layouts/modals/ClienteleModal';
+import SubscribersModal from '@/layouts/modals/SubscribersModal';
 import SubscriptionsModal from '@/layouts/modals/SubscriptionsModal';
 import MyProfessionalsModal from '@/layouts/modals/MyProfessionalsModal';
 
-// import { useAuth } from '@/auth_context';
+import { useAuth } from '@/auth_context';
 import { useUsers } from '@/users_context';
 import { useDisclosure } from '@chakra-ui/react';
 
 const Professionals = () => {
-	// const { userRole } = useAuth();
-	// const { navLinks: professional } = useUsers('professional');
-	const { navLinks } = useUsers('client');
+	const { userRole } = useAuth();
+	const { navLinks } = useUsers(userRole);
 
-	// const links = userRole === 'client' ? client : professional;
+	const professionalModal = {
+		clienteleModal: useDisclosure(),
+		subscribersModal: useDisclosure(),
+	};
 
-	// const modals = {
-	// 	clienteleModal: useDisclosure(),
-	// 	subscribersModal: useDisclosure(),
-	// };
-
-	const modals = {
+	const clientModal = {
 		subscriptionsModal: useDisclosure(),
 		myProfessionalsModal: useDisclosure(),
 	};
+
+	const modals = userRole === 'professional' ? professionalModal : clientModal;
+
 	return (
 		<main className={styles.main}>
 			<Head>
@@ -33,12 +35,20 @@ const Professionals = () => {
 			</Head>
 			<Navbar styles={styles} modals={modals} links={navLinks(modals)} />
 			<Layout />
-			{modals && modals.subscriptionsModal.isOpen && (
-				<SubscriptionsModal {...modals.subscriptionsModal} />
-			)}
-			{modals && modals.myProfessionalsModal.isOpen && (
-				<MyProfessionalsModal {...modals.myProfessionalsModal} />
-			)}
+
+			{userRole === 'client'
+				? (modals && modals.subscriptionsModal.isOpen && (
+						<SubscriptionsModal {...modals.subscriptionsModal} />
+				  )) ||
+				  (modals && modals.myProfessionalsModal.isOpen && (
+						<MyProfessionalsModal {...modals.myProfessionalsModal} />
+				  ))
+				: (modals && modals.clienteleModal.isOpen && (
+						<ClienteleModal {...modals.clienteleModal} />
+				  )) ||
+				  (modals && modals.subscribersModal.isOpen && (
+						<SubscribersModal {...modals.subscribersModal} />
+				  ))}
 		</main>
 	);
 };
