868978250d7c19de5e50f04e8383da4321bbcc39
Godfrey Peralta <godfreydjperalta@gmail.com>
2022-05-24 13:49:41 +0800

    Add data and isloading to useQuery

--- src/layouts/modals/ClienteleModal.jsx
+++ src/layouts/modals/ClienteleModal.jsx
@@ -1,37 +1,41 @@
-import Modal from "@/components/overlay/Modal"
-import AlertDialog from "@/components/overlay/AlertDialog"
-import UserConnectionList from "../connections/UserConnectionList"
+import Modal from '@/components/overlay/Modal';
+import AlertDialog from '@/components/overlay/AlertDialog';
+import UserConnectionList from '../connections/UserConnectionList';
 
-import {useQuery} from "react-query"
-import {useDisclosure} from "@chakra-ui/react"
-import {useConnections} from "@/connections_context"
+import { useQuery } from 'react-query';
+import { useDisclosure } from '@chakra-ui/react';
+import { useConnections } from '@/connections_context';
 
-function ClienteleModal({...props}) {
-  const deleteAlertDialog = useDisclosure()
-  const {getClientele} = useConnections()
+function ClienteleModal({ ...props }) {
+	const deleteAlertDialog = useDisclosure();
+	const { getClientele } = useConnections();
 
-  useQuery("clientele", getClientele)
+	const { data, isLoading } = useQuery('clientele', getClientele);
 
-  const handleDelete = () => {
-    console.log("delete connection")
-  }
+	const handleDelete = () => {
+		console.log('delete connection');
+	};
 
-  return (
-    <Modal {...props} header="Clietele">
-      <UserConnectionList deleteAlertDialog={deleteAlertDialog}/>
+	return (
+		<Modal {...props} header="Clietele">
+			<UserConnectionList
+				connections={data}
+				isLoading={isLoading}
+				deleteAlertDialog={deleteAlertDialog}
+			/>
 
-      {/* Delete Connection Alert */}
-      <AlertDialog 
-        isCentered
-        buttonColor="red"
-        buttonLabel="Delete"
-        {...deleteAlertDialog}
-        header="Delete Clientele?"
-        buttonClick={handleDelete}
-        label="Are you sure? You can't undo this action afterwards."
-      />
-    </Modal>
-  )
+			{/* Delete Connection Alert */}
+			<AlertDialog
+				isCentered
+				buttonColor="red"
+				buttonLabel="Delete"
+				{...deleteAlertDialog}
+				header="Delete Clientele?"
+				buttonClick={handleDelete}
+				label="Are you sure? You can't undo this action afterwards."
+			/>
+		</Modal>
+	);
 }
 
-export default ClienteleModal
+export default ClienteleModal;
